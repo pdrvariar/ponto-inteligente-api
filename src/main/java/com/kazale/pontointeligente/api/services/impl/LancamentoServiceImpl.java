619a1db6f6a13a71110a122b649c55116a603eeb
pdrvariar <pdrvariar@gmail.com>
2021-07-15 17:14:45 -0300

    Adicionado ehCache ao projeto

--- src/main/java/com/kazale/pontointeligente/api/services/impl/LancamentoServiceImpl.java
+++ src/main/java/com/kazale/pontointeligente/api/services/impl/LancamentoServiceImpl.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -27,12 +29,14 @@ public class LancamentoServiceImpl implements LancamentoService {
 		return this.lancamentoRepository.findByFuncionarioId(funcionarioId, pageRequest);
 	}
 
+	@Cacheable("lancamentoPorId")
 	@Override
 	public Optional<Lancamento> buscarPorId(Long id) {
 		log.info("Buscando um lançamento pelo ID {}", id);
 		return this.lancamentoRepository.findById(id);
 	}
 
+	@CachePut("lancamentoPorId")
 	@Override
 	public Lancamento persistir(Lancamento lancamento) {
 		log.info("Persistindo lancamento: {}", lancamento);
